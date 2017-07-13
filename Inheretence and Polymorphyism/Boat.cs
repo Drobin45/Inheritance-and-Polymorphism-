b21e7dbfc9c167954809d25383a590fb1bc3a3e1
drobin45 <drobin45@kent.edu>
2017-07-13 13:03:50 -0400

    Added classes

---
+++ Inheretence and Polymorphyism/Boat.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheretence_and_Polymorphyism
+{
+    class Boat : Vehicle
+    {
+        // To make one class the child of another, you only need to place a ':' and add the parent class after.
+
+        private double waterDrag;
+
+        public Boat(int seats, int carryingCapacity, string color, double movementSpeed, double waterDrag)
+        {
+            this.seats = seats;
+            this.carryingCapacity = carryingCapacity;
+            this.color = color;
+            this.movementSpeed = movementSpeed;
+            this.waterDrag = waterDrag;
+        }
+
+        //Override allows you to take a method from a parent class and make it unique to this class.
+        // In this example, we are changing movement, to include water drag, because that is a unique movement calculation to boats.
+        public override void Move()
+        {
+            distanceTraveled += movementSpeed * waterDrag;
+        }
+
+
+    }
+}
--- Inheretence and Polymorphyism/Program.cs
+++ Inheretence and Polymorphyism/Program.cs
@@ -11,7 +11,10 @@ namespace Inheretence_and_Polymorphyism
         static void Main(string[] args)
         {
 
-
+            Boat boat1 = new Boat(2, 500, "White and Yellow", 70.0d, .78d);
+            boat1.Move();
+            boat1.Move();
+            Console.WriteLine("Boat" + boat1.GetDistanceTraveled());
 
         }
     }
